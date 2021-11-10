7da76ec83682fc232178e18cfacb730532b44cfe
Beasley <63673533+beasleyDOTcom@users.noreply.github.com>
2021-11-10 15:53:43 -0800

    Add project files.

---
+++ Performer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetTwilioAppOnHerokuViaDocker
+{
+    public class Performer
+    {
+        public string phoneNumber { get; set; }
+
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetTwilioAppOnHerokuViaDocker
+{
+    public class Room
+    {
+        public string roomCode { get; set; }
+        public string host { get; set; }
+        public List<Performer> participants { get; set; }
+    }
+}
